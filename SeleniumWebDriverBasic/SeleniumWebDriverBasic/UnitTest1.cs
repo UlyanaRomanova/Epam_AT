fd7bf71d6ed51a6e1a7289424e425e02fefc0c2f
Ulyana <RomanovaUY@gmail.com>
2020-12-16 02:14:53 +0300

    WebDriverBasic. исправление
    Вынесла авторизацию и переход на вкладку с продуктами в отдельные методы. Метод авторизации запускается в SetUp. Добавила порядок запуска тестов. Убрала Создание продукта при его отсутствии.

--- SeleniumWebDriverBasic/SeleniumWebDriverBasic/UnitTest1.cs
+++ SeleniumWebDriverBasic/SeleniumWebDriverBasic/UnitTest1.cs
@@ -10,8 +10,8 @@ namespace SeleniumWebDriverBasic
     {
         private static IWebDriver driver;
         
-        // Проверка наличия элемента на странице. Метод.
-        public static bool IsElementPresent(By locator) 
+        // Check an element is present. Method.
+        private static bool IsElementPresent(By locator) 
         {
             try
             {
@@ -22,6 +22,19 @@ namespace SeleniumWebDriverBasic
                 return false;
             }
         }
+
+        private void Authorization()
+        {
+            driver.FindElement(By.Id("Name")).SendKeys("user");
+            driver.FindElement(By.Id("Password")).SendKeys("user");
+            driver.FindElement(By.CssSelector(".btn")).Click();
+        }
+
+        // Go to "All Products"
+        private void ClickAllProducts()
+        {
+            driver.FindElement(By.XPath("(//a[contains(@href,'/Product')])[2]")).Click();
+        }
                 
         [SetUp]
         public void Setup()
@@ -29,42 +42,28 @@ namespace SeleniumWebDriverBasic
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl("http://localhost:5000");
             driver.Manage().Window.Maximize();
-            
+            Authorization();
         }
 
-        [Test]
+        [Test, Order(1)]
         public void LoginTest()
         {
-            // Заполнение полей авторизации.
-            driver.FindElement(By.Id("Name")).SendKeys("user");
-            driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-
-            // Проверка наличия элемента "Logout" на странице.
+            // Check "Logout"-element.
             bool ElementLogout = IsElementPresent(By.XPath("//header//*[@href='/Account/Logout']"));
             Assert.IsTrue(ElementLogout);
-            
         }
 
-        [Test]
+        [Test, Order(2)]
         public void AddNewProduct()
         {
-            // Авторизация.
-            driver.FindElement(By.Id("Name")).SendKeys("user");
-            driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
+            ClickAllProducts();
 
-            // Переход по ссылке "All Products".
-            driver.FindElement(By.XPath("(//a[contains(@href,'/Product')])[2]")).Click();
-
-            // Проверка наличия элемента "Create new" на странице. 
+            // Check element "Create new". 
             bool ButtonCreate = IsElementPresent(By.XPath("//a[contains(text(),'Create new')]"));
             Assert.IsTrue(ButtonCreate);
 
-            // Создание нового продукта.
+            // Сreate new product.
             driver.FindElement(By.LinkText("Create new")).Click();
-            
-            // Заполнение полей.
             driver.FindElement(By.Id("ProductName")).SendKeys("Coffee");
             
             IWebElement CategoryElement = driver.FindElement(By.Id("CategoryId"));
@@ -81,53 +80,22 @@ namespace SeleniumWebDriverBasic
             driver.FindElement(By.Id("UnitsOnOrder")).SendKeys("3");
             driver.FindElement(By.Id("ReorderLevel")).SendKeys("5");
             driver.FindElement(By.Id("Discontinued")).Click();
-
-            // Нажатие кнопки "отправить". Отправка данных.
             driver.FindElement(By.CssSelector(".btn")).Click();
 
-            // Проверка закрытия формы после создания продукта. Отсутствие элемента "кнопка "отправить".
+            // Check form close. Button "submit" is not present.
             bool ButtonSend = IsElementPresent(By.XPath("//input[@type='submit']"));
             Assert.IsFalse(ButtonSend);
         }
 
-        [Test]
+        [Test, Order(3)]
         public void OpenProduct()
         {
-            
-            // Авторизация и переход по ссылке "All Products".
-            driver.FindElement(By.Id("Name")).SendKeys("user");
-            driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-            driver.FindElement(By.XPath("(//a[contains(@href,'/Product')])[2]")).Click();
-
-            // Проверка наличия созданного продукта "Coffee". Если нет, создать.
-            bool CoffeElementPresent = IsElementPresent(By.LinkText("Coffee"));
-            if (!CoffeElementPresent)
-            {
-                // Создание нового объекта "Coffee"
-                driver.FindElement(By.LinkText("Create new")).Click();
-                // Заполнение полей.
-                driver.FindElement(By.Id("ProductName")).SendKeys("Coffee");
-                IWebElement CategoryElement = driver.FindElement(By.Id("CategoryId"));
-                SelectElement categorySelect = new SelectElement(CategoryElement);
-                categorySelect.SelectByText("Beverages");
-                IWebElement SupplierElement = driver.FindElement(By.Id("SupplierId"));
-                SelectElement supplierSelect = new SelectElement(SupplierElement);
-                supplierSelect.SelectByText("Exotic Liquids");
-                driver.FindElement(By.Id("UnitPrice")).SendKeys("25");
-                driver.FindElement(By.Id("QuantityPerUnit")).SendKeys("5 boxes x 10 bags");
-                driver.FindElement(By.Id("UnitsInStock")).SendKeys("32");
-                driver.FindElement(By.Id("UnitsOnOrder")).SendKeys("3");
-                driver.FindElement(By.Id("ReorderLevel")).SendKeys("5");
-                driver.FindElement(By.Id("Discontinued")).Click();
-                // Отправка данных формы.
-                driver.FindElement(By.CssSelector(".btn")).Click();
-            }
-
-            // Выбор продукта.
+            ClickAllProducts();
+                        
+            // Select "Coffee".
             driver.FindElement(By.LinkText("Coffee")).Click();
             
-            // Проверки заполненности полей.
+            // Check fill fields.
             Assert.IsNotEmpty(driver.FindElement(By.Id("ProductId")).GetAttribute("value"));
             Assert.AreEqual("Coffee", driver.FindElement(By.Id("ProductName")).GetAttribute("value"));
             Assert.AreEqual("1", driver.FindElement(By.Id("CategoryId")).GetAttribute("value"));
@@ -141,57 +109,26 @@ namespace SeleniumWebDriverBasic
 
         }
 
-        [Test]
+        [Test, Order(4)]
         public void DeleteProductTest()
         {
+            ClickAllProducts();
 
-            // Авторизация и переход по ссылке "All Products".
-            driver.FindElement(By.Id("Name")).SendKeys("user");
-            driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-            driver.FindElement(By.XPath("(//a[contains(@href,'/Product')])[2]")).Click();
-
-            bool CoffeElementPresent = IsElementPresent(By.LinkText("Coffee"));
-            if (CoffeElementPresent == false) 
-            {
-                // Создание нового объекта "Coffee"
-                driver.FindElement(By.LinkText("Create new")).Click();
-                // Заполнение полей.
-                driver.FindElement(By.Id("ProductName")).SendKeys("Coffee");
-                IWebElement CategoryElement = driver.FindElement(By.Id("CategoryId"));
-                SelectElement categorySelect = new SelectElement(CategoryElement);
-                categorySelect.SelectByText("Beverages");
-                IWebElement SupplierElement = driver.FindElement(By.Id("SupplierId"));
-                SelectElement supplierSelect = new SelectElement(SupplierElement);
-                supplierSelect.SelectByText("Exotic Liquids");
-                driver.FindElement(By.Id("UnitPrice")).SendKeys("25");
-                driver.FindElement(By.Id("QuantityPerUnit")).SendKeys("5 boxes x 10 bags");
-                driver.FindElement(By.Id("UnitsInStock")).SendKeys("32");
-                driver.FindElement(By.Id("UnitsOnOrder")).SendKeys("3");
-                driver.FindElement(By.Id("ReorderLevel")).SendKeys("5");
-                driver.FindElement(By.Id("Discontinued")).Click();
-                // Отправка данных формы.
-                driver.FindElement(By.CssSelector(".btn")).Click();
-            }
+            // Check element "Remove" for element "Coffee"
+            bool RemoveCoffeElement = IsElementPresent(By.XPath("(//*[a='Coffee']/following-sibling::*[a='Remove']/a)[1]"));
+            Assert.IsTrue(RemoveCoffeElement);
 
             // Для сравнения по ProductID при наличии нескольких элементов "Coffee".
             string StrProductIDBefore = driver.FindElement(By.XPath("(//*[a='Coffee']/preceding-sibling::td)[1]")).Text;
             int ProductIdBefore = int.Parse(StrProductIDBefore);
 
-            // Проверка наличия элемента "Remove" для элемента "Coffee"
-            bool RemoveCoffeElement = IsElementPresent(By.XPath("(//*[a='Coffee']/following-sibling::*[a='Remove']/a)[1]"));
-            Assert.IsTrue(RemoveCoffeElement);
-            
-            // Выбрать Удалить элемент. 
+            // Remove. 
             driver.FindElement(By.XPath("(//*[a='Coffee']/following-sibling::*[a='Remove']/a)[1]")).Click();
-            // "Ok" при всплывающем предeпреждении.
             driver.SwitchTo().Alert().Accept();
-
             driver.Navigate().Refresh();
             driver.Navigate().Refresh();
-            // Проверка наличия продукта "Coffee" на странице.
 
-            // Если эементов "Coffee" несколько, то сравнить удаленный по ProductID.
+            // Check "Coffee" is present.
             bool CoffeElementAfter = IsElementPresent(By.LinkText("Coffee"));
             if (CoffeElementAfter)
             {
@@ -201,43 +138,34 @@ namespace SeleniumWebDriverBasic
             }
             else 
             {
-                // Проверка на отсутствие элемента "Coffee"
                 Assert.IsFalse(CoffeElementAfter);
             }
             
 
         }
 
-        [Test]
+        [Test, Order(5)]
         public void LogOutTest()
         {
-            // Авторизация и переход по ссылке "All Products".
-            driver.FindElement(By.Id("Name")).SendKeys("user");
-            driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-
-            // Проверка наличия элемента "Logout" на странице.
+            // Check "Logout" is present.
             bool ElementLogout = IsElementPresent(By.XPath("//header//*[@href='/Account/Logout']"));
             Assert.IsTrue(ElementLogout);
 
             // Logout.
             driver.FindElement(By.LinkText("Logout")).Click();
 
-            // Проверка наличия элементов ввода "Name" и "Password" на странице. Начальная страница авторизации.
+            // Check authorization page. 
             bool ElementLogInName = IsElementPresent(By.Id("Name"));
             bool ElementLogInPassword = IsElementPresent(By.Id("Password"));
             Assert.IsTrue(ElementLogInName);
             Assert.IsTrue(ElementLogInPassword);
         }
 
-
-
-            [TearDown]
+        [TearDown]
         public void CloseDriver()
         {
             driver.Close();
             driver.Quit();
-
         }
 
     }
